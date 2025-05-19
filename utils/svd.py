06f85f15afab82bdf1ad3426c5998c544a48ba18
slightblur <abdullahshah9995@gmail.com>
2025-05-19 21:02:24 +0500

    Deleted legacy model and utilities

--- utils/svd.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def compute_svd(matrix):
-    U, S, Vt = np.linalg.svd(matrix)
-    return U, S, Vt
-
-def compute_eigen(matrix):
-    values, vectors = np.linalg.eig(matrix)
-    return values, vectors
